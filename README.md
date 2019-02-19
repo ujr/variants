9f3f9eed8a3f8dad8fc8ed164d8afe4371531013
Urs-Jakob <ujr@bernina>
2019-02-19 16:05:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,123 @@
+
+
+Variant Expansion
+=================
+
+Generate variants of an input string based on markup:
+the vertical bar separates variants, brackets limit the scope of
+variants, and brackets with only one variant (i.e., no vertical
+bar) denote an optional part.
+
+Empty variants are not emitted.  
+Brackets can be nested, but should be balanced.  
+Invalid markup may result in unexpected output but never gives an error.
+
+Examples:
+
+ * `"foo|bar"` expands into the two strings `"foo"` and `"bar"`
+ * `"ba[r|z]"` expands into the two strings `"bar"` and `"baz"`
+ * `"qu[u]x"` expands into `"quux"` and `"qux"`
+ * `"foo|ba[r|z[aar]]|quux"` expands
+   into `"foo"`, `"bar"`, `"bazaar"`, `"baz"`, `"quux"`
+ * `"[foo|]"` expands to `"foo"` (the empty string is not emitted)
+
+Limitations:
+
+ * No escaping is possible, that is, brackets and the vertical bar
+   are always interpreted with their special meaning.
+
+
+Syntax
+------
+
+The syntax of variant notation is as follows:
+
+```
+Variants = Sequence { '|' Sequence }  
+Sequence = Part { Part }  
+Part = Text | '[' Variants ']'
+```
+
+Here *Text* is any characters other than the vertical bar or brackets,
+{...} denotes repetition (zero or more times), and | separates alternatives.
+
+
+Usage
+-----
+
+From a shell prompt, type `make` to compile the C source,
+which should result in the `variants` executable program.
+This program reads variant notation from all its arguments,
+one after the other, and writes the expansions to standard output.
+Be careful to quote arguments because the special characters of variant
+syntax have special meaning to your shell, too.
+
+Usage: **variants** _text_ ...
+
+Example run:
+
+```
+$ expand "ba[r|z[aar]]" qu[u]x
+bar
+bazaar
+baz
+quux
+qux
+```
+
+
+Miscellaneous
+-------------
+
+The implementation works by traversing the dag defined by the markup
+as though it was a tree. For example, the input `"a[1|2]b"` defines
+the dag in Fig 1, which for expansion is traversed as though it was
+the tree in Fig 2. A preliminary parsing step creates two arrays,
+down and next, with link indices that facilitate navigation on the dag:
+in Fig 3, | and / are down links, and -- is a next link.
+
+          a                   a                   a
+         / \                 / \                  |
+        1   2               1   2                 1--2
+         \ /                |   |                 | /
+          b    Fig.1        b   b   Fig.2         b     Fig.3
+
+Some more examples:
+
+ * `It[[em|alic]iz|erat]e[d], please` (from [Rosetta Code][rc])
+ * `a[1|2|3]b[4|5]c` (yields 3 x 2 = 6 variants)
+ * `J[oh[ann]] W[olfgang] [v[on] ]Goethe`
+
+Weird and invalid syntax examples:
+
+ * `[[[foo]]]` yields only "foo" (because empty variants are not emitted)
+ * `[|||]` yields no output (because empty variants are not emitted)
+ * `f]o|o]` yields "fo" and "o" (unmatched closing brackets are ignored)
+ * `[f[o[o` yields "foo", "fo", "f"
+    (unmatched opening brackets are implicitly closed at end of input)
+ * `x[]x` and `xx[` both yield "xx" and "xx"
+    ([] is equivalent to [|] and the unmatched bracket is implicitly closed)
+ * `x[|]x` yields 2 times "xx"
+ * `x[[]]x` yields 3 times "xx"
+
+Note that Variant Expansion is the same idea as *Brace Expansion* 
+in Unix shells (e.g. in [Bash][bash]), but with a different syntax.
+Also, brace expansion usually has an extra feature called range
+expansion, and brace expansion is only one of many types of expansion
+supported by shells (see [here][lcorg]). Implementations of brace
+expansion in many languages can be found at [Rosetta Code][rc].
+
+[rc]: https://rosettacode.org/wiki/Brace_expansion
+[bash]: https://www.gnu.org/software/bash/manual/html_node/Brace-Expansion.html
+[lcorg]: http://linuxcommand.org/lc3_lts0080.php
+
+
+License
+-------
+
+MIT
+
+See the LICENSE file (or read the license text
+at [choosealicense.com](https://choosealicense.com/licenses/mit/)
+or [opensource.org](https://opensource.org/licenses/MIT)).
+
---
+++ variants.c
@@ -0,0 +1,347 @@
+/* Variant Expansion * see README.md and LICENSE */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define EXIT_MEM 1  /* cannot allocate memory */
+
+static char *me = "variants";
+
+/*** Stack ***/
+
+typedef struct Stack {
+  int *data;
+  int size, top;
+} Stack;
+
+static void stack_init(Stack *pStk, int initsize);
+static void stack_grow(Stack *pStk, int newsize);
+static void stack_free(Stack *pStk);
+static void stack_push(Stack *pStk, int value);
+static int stack_peek(Stack *pStk);
+static int stack_pop(Stack *pStk);
+static int stack_count(Stack *pStk);
+
+static void stack_init(Stack *pStk, int initsize)
+{
+  assert(pStk != NULL);
+  pStk->data = NULL;
+  pStk->size = 0;
+  pStk->top = 0;
+  stack_grow(pStk, initsize);
+}
+
+static void stack_grow(Stack *pStk, int newsize)
+{
+  int *newdata;
+  assert(pStk != NULL);
+  if (newsize < 4) newsize = 4;
+  newdata = realloc(pStk->data, newsize);
+  if (!newdata) {
+    perror("Cannot stack_grow");
+    exit(EXIT_MEM);
+  }
+  pStk->data = newdata;
+  pStk->size = newsize;
+}
+
+static void stack_free(Stack *pStk)
+{
+  assert(pStk != NULL);
+  if (pStk->data) free(pStk->data);
+  pStk->data = NULL;
+  pStk->size = 0;
+  pStk->top = 0;
+}
+
+static void stack_push(Stack *pStk, int value)
+{
+  assert(pStk != NULL);
+  if(pStk->top >= pStk->size){
+    stack_grow(pStk, 2*pStk->size);
+  }
+  pStk->data[pStk->top++] = value;
+}
+
+static int stack_peek(Stack *pStk)
+{
+  assert(pStk != NULL);
+  assert(pStk->top > 0);
+  return pStk->data[pStk->top-1];
+}
+
+static int stack_pop(Stack *pStk)
+{
+  assert(pStk != NULL);
+  assert(pStk->top > 0);
+  return pStk->data[--pStk->top];
+}
+
+static int stack_count(Stack *pStk)
+{
+  assert(pStk != NULL);
+  return pStk->top;
+}
+
+/*** Buffer ***/
+
+typedef struct Buffer {
+  char *data;
+  int size, len;
+} Buffer;
+
+static void buffer_init(Buffer *pBuf, int initsize);
+static void buffer_grow(Buffer *pBuf, int newsize);
+static void buffer_free(Buffer *pBuf);
+static void buffer_append(Buffer *pBuf, char c);
+static void buffer_truncate(Buffer *pBuf, int len);
+static int buffer_length(Buffer *pBuf);
+static const char *buffer_string(Buffer *pBuf); // ptr to \0 term'd string
+
+static void buffer_init(Buffer *pBuf, int initsize)
+{
+  assert(pBuf!=NULL);
+  pBuf->data = NULL;
+  pBuf->size = 0;
+  pBuf->len = 0;
+  buffer_grow(pBuf, initsize);
+}
+
+static void buffer_grow(Buffer *pBuf, int newsize)
+{
+  char *newdata;
+  assert(pBuf!=NULL);
+  if (newsize < 4) newsize = 4;
+  newdata = realloc(pBuf->data, newsize);
+  if (!newdata) {
+    perror("Cannot buffer_grow");
+    exit(EXIT_MEM);
+  }
+  pBuf->data = newdata;
+  pBuf->size = newsize;
+}
+
+static void buffer_free(Buffer *pBuf)
+{
+  assert(pBuf!=NULL);
+  if(pBuf->data) free(pBuf->data);
+  pBuf->data = NULL;
+  pBuf->size = 0;
+  pBuf->len = 0;
+}
+
+static void buffer_append(Buffer *pBuf, char c)
+{
+  assert(pBuf!=NULL);
+  if(pBuf->len >= pBuf->size){
+    buffer_grow(pBuf, 2*pBuf->size);
+  }
+  pBuf->data[pBuf->len++] = c;
+}
+
+static void buffer_truncate(Buffer *pBuf, int len)
+{
+  assert(pBuf!=NULL);
+  assert(len<=pBuf->len);
+  pBuf->len = len;
+}
+
+static int buffer_length(Buffer *pBuf)
+{
+  assert(pBuf!=NULL);
+  return pBuf->len;
+}
+
+static const char *buffer_string(Buffer *pBuf)
+{
+  assert(pBuf!=NULL);
+  if(pBuf->data[pBuf->len] != '\0'){
+    buffer_append(pBuf, '\0');
+  }
+  return pBuf->data;
+}
+
+/*** Debugging ***/
+
+static void
+printposn(const char *prefix, int n)
+{
+  int i;
+  if (prefix) printf("%s:", prefix);
+  for (i=0; i<n; i++) printf(" %X", i);
+  printf("\n");
+}
+
+static void
+printtext(const char *prefix, const char *s, int n)
+{
+  int i;
+  if (prefix) printf("%s:", prefix);
+  for (i=0; i<n; i++) printf(" %c", s[i]);
+  printf("\n");
+}
+
+static void
+printlnks(const char *prefix, const int *a, int n)
+{
+  int i;
+  if (prefix) printf("%s:", prefix);
+  for (i=0; i<n; i++) printf(a[i]?" %X":" -", a[i]);
+  printf("\n");
+}
+
+/*** Variant Expansion ***/
+
+static void expand(const char *text, int len);
+static int parse(const char *text, int len, int *down, int *next);
+static void traverse(const char *text, int len,
+                     const int *down, const int *next, int i, Buffer *buf);
+
+static void
+expand(const char *text, int len)
+{
+  int pipe, reset;
+  int *down, *next;
+  Buffer buf;
+
+  down = calloc(len, sizeof(int));
+  if (!down) { exit(EXIT_MEM); }
+  next = calloc(len, sizeof(int));
+  if (!next) { free(down); exit(EXIT_MEM); }
+
+  // posn: 0 1 2 3 4 5 6 7 8 9 A B
+  // text: a [ b [ c ] | d | e ] $
+  // down: : B : 6 : : B : B : :     one after closing bracket
+  // down: 1 : 3 : 5 6 : 8 : A B     down[i] > i for all i
+  // next: - 6 - - - - 8 - - - -     index of next pipe
+
+  pipe = parse(text, len, down, next);
+
+//  printposn("posn", len);
+//  printtext("text", text, len);
+//  printlnks("down", down, len);
+//  printlnks("next", next, len);
+
+  buffer_init(&buf, len);
+
+  // Expand the first top-level variant:
+  reset = buffer_length(&buf);
+  traverse(text, len, down, next, 0, &buf);
+
+  // Expand remaining top-level variants:
+  while (pipe > 0) {
+    buffer_truncate(&buf, reset);
+    traverse(text, len, down, next, pipe+1, &buf);
+    pipe = next[pipe];
+  }
+
+  buffer_free(&buf);
+
+  free(next);
+  free(down);
+}
+
+static int
+parse(const char *text, int len, int *down, int *next)
+{
+  Stack stk;                     // stack of OPEN and PIPE indices
+  int first_top_level_pipe = -1; // index of PIPE outside OPEN/CLOSE
+  int i;                         // index into text
+
+  stack_init(&stk, 0);
+
+  for (i=0; i < len; i++) {
+    switch (text[i]) {
+      case '[':
+        stack_push(&stk, i); // remember bracket position
+        break;
+      case '|':
+        if (stack_count(&stk) > 0) {
+          int j = stack_peek(&stk);
+          next[j] = i;
+        }
+        else {
+          first_top_level_pipe = i; // next[-1] = i
+        }
+        stack_push(&stk, i); // remember pipe position
+        break;
+      case ']':
+        while (stack_count(&stk) > 0) {
+          int j = stack_pop(&stk);
+          down[j] = i+1;
+          if (text[j]=='[') break;
+        }
+        down[i] = i+1;
+        break;
+      default:
+        down[i] = i+1;
+        break;
+    }
+  }
+
+  // Implicitly close unmatched opening brackets at end of text:
+
+  while (stack_count(&stk) > 0) {
+    int j = stack_pop(&stk);
+    down[j] = i;
+  }
+
+  stack_free(&stk);
+
+  return first_top_level_pipe;
+}
+
+static void
+traverse(const char *text, int len, const int *down, const int *next, int i, Buffer *buf)
+{
+  int reset;
+
+  while (i < len) {
+    switch (text[i]) {
+    case '[':
+      reset = buffer_length(buf); // remember prefix length
+      traverse(text, len, down, next, i+1, buf);
+      if (next[i] <= 0) { // optional part: [x]
+        buffer_truncate(buf, reset); // truncate to prefix
+        traverse(text, len, down, next, down[i], buf);
+      }
+      else while (next[i] > 0) { // variants: [x|...]
+        i = next[i];
+        buffer_truncate(buf, reset); // truncate to prefix
+        traverse(text, len, down, next, i+1, buf);
+      }
+      return; // all down by recursive calls
+    case '|':
+      i = down[i];
+      break;
+    case ']':
+      i += 1;
+      break;
+    default:
+      buffer_append(buf, text[i]);
+      i += 1;
+      break;
+    }
+  }
+
+  if (buffer_length(buf) > 0) {
+    puts(buffer_string(buf)); // emit non-empty variant
+  }
+}
+
+int
+main(int argc, char **argv)
+{
+  if (argv && *argv) me = *argv++;
+  else return 127; // invalid invocation
+
+  while (*argv) {
+    int len = strlen(*argv);
+    expand(*argv, len);
+    argv++; argc--; // shift
+  }
+
+  return 0;
+}
